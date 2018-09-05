64d98112bbe6f11759499e7cca09bc15bdb1909d
Tobe O <thosakwe@gmail.com>
2018-09-05 13:42:45 -0400

    Spits out version information on failure

---
+++ packages/build_aot/lib/build_aot.dart
@@ -0,0 +1,4 @@
+import 'package:build/build.dart';
+import 'src/dill_to_c_builder.dart';
+
+Builder dillToCBuilder(_) => const DillToCBuilder();
--- packages/build_aot/lib/src/dill_to_c_builder.dart
+++ packages/build_aot/lib/src/dill_to_c_builder.dart
@@ -2,10 +2,13 @@ import 'dart:async';
 
 import 'package:build/build.dart';
 import 'package:code_buffer/code_buffer.dart';
+import 'package:kernel/binary/ast_from_binary.dart';
 import 'package:kernel/kernel.dart';
 import 'compiler/compiler.dart';
 
 class DillToCBuilder implements Builder {
+  const DillToCBuilder();
+
   @override
   Map<String, List<String>> get buildExtensions {
     return const {
@@ -15,13 +18,17 @@ class DillToCBuilder implements Builder {
 
   @override
   Future build(BuildStep buildStep) async {
-    var bytes = await buildStep.readAsBytes(buildStep.inputId);
-    var component = await loadComponentFromBytes(bytes);
-    var compiler = new DillToCCompiler();
-    var buffer = new CodeBuffer();
-    var outputId = buildStep.inputId.changeExtension('.c');
-    component.accept(compiler);
-    compiler.compilationUnit.generate(buffer);
-    await buildStep.writeAsString(outputId, buffer.toString());
+    try {
+      var bytes = await buildStep.readAsBytes(buildStep.inputId);
+      var component = await loadComponentFromBytes(bytes);
+      var compiler = new DillToCCompiler();
+      var buffer = new CodeBuffer();
+      var outputId = buildStep.inputId.changeExtension('.c');
+      component.accept(compiler);
+      compiler.compilationUnit.generate(buffer);
+      await buildStep.writeAsString(outputId, buffer.toString());
+    } on InvalidKernelVersionError catch (e) {
+      throw e.message;
+    }
   }
 }
